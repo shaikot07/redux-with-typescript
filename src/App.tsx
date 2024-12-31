5dbdfc846d5d8497c01088fefaf1be88d25b3767
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-31 22:17:12 +0600

    ply react with redux-toolkit and typeScript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css"; // Import Tailwind CSS
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-[#F3E8FF] p-8">
+        <h2 className=" text-center text-2xl font-semibold">
+          Explore The <span className="text-[#7B5EA7]">Redux-Toolkit</span> With <span className="text-[#3178C6]"> TypeScript</span>
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
