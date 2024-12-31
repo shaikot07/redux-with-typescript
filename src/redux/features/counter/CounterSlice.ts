b055e220c35393bed28e72f15e118d993a8c5b18
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-31 22:53:22 +0600

    build logic for the increment and decrement

--- src/redux/features/counter/CounterSlice.ts
+++ src/redux/features/counter/CounterSlice.ts
@@ -8,7 +8,12 @@ const counterSlices= createSlice({
     name:'counter',
     initialState,
     reducers:{
-
+        increment:(state)=>{
+            state.count =state.count + 1
+        },
+        decrement:(state)=>{
+            state.count =state.count - 1
+        }
     }
 })
 
